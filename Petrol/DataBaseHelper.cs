fce46f1889e8d484dca483bfa601fd53b5b255d6
Mehmet Çoban <137454414+coban9066@users.noreply.github.com>
2024-12-12 23:57:31 +0300

    V1

---
+++ Petrol/DataBaseHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Petrol
+{
+    internal class DataBaseHelper
+    {
+        private readonly string connectionString = "Data Source=DESKTOP-N5LCA8G;Initial Catalog=petrol;Integrated Security=True;Encrypt=False;";
+
+        public SqlConnection GetConnection()
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open(); 
+                return connection;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Bağlantı sırasında bir hata oluştu: " + ex.Message);
+            }
+        }
+
+        public void CloseConnection(SqlConnection connection)
+        {
+            if (connection != null && connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+    }
+}
---
+++ Petrol/Form2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Petrol
+{
+    public partial class Form2 : Form
+    {
+        private readonly DataBaseHelper dbHelper = new DataBaseHelper();
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string subeAdi = textBox1.Text; 
+
+            if (string.IsNullOrWhiteSpace(subeAdi))
+            {
+                MessageBox.Show("Lütfen bir şube adı giriniz!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                using (SqlConnection connection = dbHelper.GetConnection())
+                {
+                    string query = "SELECT ad, soyad, yetki FROM kullanici WHERE sube = @sube";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@sube", subeAdi);
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            listBox1.Items.Clear(); 
+                            listBox2.Items.Clear(); 
+
+                            while (reader.Read())
+                            {
+                                string ad = reader["ad"].ToString();
+                                string soyad = reader["soyad"].ToString();
+                                string yetki = reader["yetki"].ToString();
+
+                                listBox1.Items.Add($"{ad} {soyad}");
+
+                                listBox2.Items.Add(yetki);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Bir hata oluştu: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
